b553dd187191f960a6d3dbafcec9e62dd4779d1d
Volkan Sönmez <hvsonmez@gmail.com>
2018-07-31 14:36:54 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,102 @@
+var menuData = [
+    {
+        text: "HTML'in Tanımı",
+        url: "index.html"
+    },
+    {
+        text: "HTML Yazma Yöntemleri",
+        url: "html_yazma_yontemleri.html"
+    },
+    {
+        text: "HTML Elemanları",
+        url: "html_elemanlari.html"
+    },
+    {
+        text: "HTML Öznitelikleri",
+        url: "html_oznitelikleri.html"
+    },
+    {
+        text: "HTML Başıkları",
+        url: "html_basliklari.html"
+    },
+    {
+        text: "HTML Paragrafları",
+        url: "html_paragraflari.html"
+    },
+    {
+        text: "HTML Bağlantıları/Linkleri",
+        url: "html_baglantilari.html"
+    },
+    {
+        text: "HTML Görüntüleri",
+        url: "html_goruntuleri.html"
+    },
+    {
+        text: "HTML Listeleri",
+        url: "html_listeleri.html"
+    },
+    {
+        text: "HTML Tabloları",
+        url: "html_tablolari.html"
+    },
+    {
+        text: "HTML Yorumları",
+        url: "html_yorumlari.html"
+    },
+    {
+        text: "HTML Biçimleri",
+        url: "#"
+    },
+    {
+        text: "HTML CSS Sınıfları",
+        url: "#"
+    },
+    {
+        text: "HTML ID'leri",
+        url: "#"
+    },
+    {
+        text: "HTML Formları",
+        url: "#"
+    },
+    {
+        text: "HTML Form Elemanları",
+        url: "#"
+    },
+    {
+        text: "HTML Form Eleman Tipleri",
+        url: "#"
+    },
+    {
+        text: "HTML Form Eleman Öznitelikleri",
+        url: "#"
+    },
+];
+var MainMenu = function (container) {
+    this.container = document.getElementById(container);
+};
+
+MainMenu.prototype.init = function(){
+    var createMenuItem = function (item) {
+        var link = document.createElement("a");
+        link.setAttribute("href", item.url);
+        link.innerHTML = item.text;
+        link.setAttribute("rel", item.url.split(".")[0]);
+        if (item.url === "#") {
+            link.classList.add("disabled");
+        }
+        if(document.body.getAttribute("rel") === link.getAttribute("rel")) {
+            link.classList.add("selected");
+        }
+        return link;
+    };
+    var menuIndex;
+    for (menuIndex = 0; menuIndex < menuData.length; menuIndex += 1) {
+        this.container.appendChild(createMenuItem(menuData[menuIndex]));
+    }
+};
+
+window.addEventListener("load", function(){
+    var myMenu = new MainMenu("main_menu");
+    myMenu.init();
+});
